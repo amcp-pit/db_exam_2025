c56b2f90a8c197d494ce7eeae188b7c4eb1afad5
nomatterwhoisthat <116099741+nomatterwhoisthat@users.noreply.github.com>
2025-01-15 02:36:21 +0300

    Add files via upload
    Обновила диаграмму и добила скрипт для удаления таблиц

---
+++ db_drop_postgre.sql
@@ -0,0 +1,15 @@
+-- Drop tables
+DROP TABLE IF EXISTS public.constraint_param CASCADE;
+DROP TABLE IF EXISTS public.constraint_info CASCADE;
+DROP TABLE IF EXISTS public.constraint CASCADE;
+DROP TABLE IF EXISTS public.constraint_type CASCADE;
+DROP TABLE IF EXISTS public.object_param CASCADE;
+DROP TABLE IF EXISTS public.object CASCADE;
+DROP TABLE IF EXISTS public.object_type CASCADE;
+DROP TABLE IF EXISTS public.param CASCADE;
+DROP TABLE IF EXISTS public.plane CASCADE;
+DROP TABLE IF EXISTS public.sketch CASCADE;
+DROP TABLE IF EXISTS public.entity CASCADE;
+DROP TABLE IF EXISTS public.access_list CASCADE;
+DROP TABLE IF EXISTS public.model CASCADE;
+DROP TABLE IF EXISTS public.user CASCADE;
